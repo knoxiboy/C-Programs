617a9f28fa70572d1071da53fea2bf3b6889eec8
knoxiboy <karankmt.tripathi@gmail.com>
2025-04-13 23:53:38 +0530

    13-04-2025

---
+++ programs/sdfgh.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int arr[7]={1,2,3,4,5};
+    printf("%d",arr[5]);
+
+    return 0;
+}
---
+++ programs/tcs2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n, m, r, days = 0;
+    scanf("%d %d", &n, &m);
+    int friendship[n][n], f_count[n];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            
+        }
+    }
+
+    return 0;
+}
